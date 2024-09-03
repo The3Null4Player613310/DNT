bc9dda1da8ace47375784ff5540f1e0be258fab7
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2024-09-03 07:08:11 +0000

    Added MainActivity.java Added MainActivity.java to the repository.

---
+++ app/src/main/java/com/thenullplayer/dnt/MainActivity.java
@@ -0,0 +1,26 @@
+//################################################################
+//#DNT: MainActivity.java
+//#Copyright © 2024 Allison Munn
+//#FULL COPYRIGHT NOTICE IS IN README
+//################################################################
+
+package com.thenullplayer.dnt;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+}
