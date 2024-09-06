6f6321707475bf6c8b470eac7698ce43c219932f
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2024-09-06 04:53:47 +0000

    Updated App Updated look and feel of app.

--- app/src/main/java/com/thenullplayer/dnt/MainActivity.java
+++ app/src/main/java/com/thenullplayer/dnt/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        
     }
 
     @Override
