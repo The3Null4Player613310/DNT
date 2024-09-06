a896da945a29cb52695ca21f075645313f689c31
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2024-09-06 08:29:53 +0000

    Updated App Updated app lifecycle. Added code to set layout.

--- app/src/main/java/com/thenullplayer/dnt/MainActivity.java
+++ app/src/main/java/com/thenullplayer/dnt/MainActivity.java
@@ -17,7 +17,39 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
 
+        //set the layout
+        setContentView(R.layout.activity_main);
         
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+    
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
     }
 
     @Override
