df4fa8bbf8f81ff7fd6d73cc65d7cd0f7b307eeb
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2024-09-10 11:52:25 +0000

    Update list.py
    Added list.py.

---
+++ list.py
@@ -0,0 +1,11 @@
+################################################################
+#DNT: list.py
+#Copyright © 2024 Allison Munn
+#FULL COPYRIGHT NOTICE IS IN README
+################################################################
+
+def main():
+    print("hello world");
+
+if __name__ == "__main__":
+    main();
