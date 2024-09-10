658aa14c78ba45e9310147fb4a8c952f01212d89
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2024-09-10 05:05:56 +0000

    Created version.py Created a program to show python version.

---
+++ version.py
@@ -0,0 +1,9 @@
+################################################################
+#DNT: version.py
+#Copyright © 2024 Allison Munn
+#FULL COPYRIGHT NOTICE IS IN README
+################################################################
+
+import sys;
+
+print(sys.version);
